29481a3f7b9443ccbb10780a2ffe3aafab868e32
Ikuo Degawa <degawa@ikuwow.com>
2016-02-17 22:32:00 +0900

    Test httpBackend

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,20 +1,23 @@
 describe('PhoneListCtrl', function(){
 
-    var scope, ctrl;
+    var scope, ctrl, $httpBackend;
 
     beforeEach(module('phonecatApp'));
 
-    beforeEach(inject(function($controller){
-        scope = {};
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
+        $httpBackend = _$httpBackend_;
+        $httpBackend.expectGET('phones/phones.json')
+            .respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+        scope = $rootScope.$new();
         ctrl = $controller('PhoneListCtrl', {$scope:scope});
     }));
 
-    it('should create "phones" model with 3 phones', inject(function($controller) {
-        expect(scope.phones.length).toBe(3);
+    it('should create "phones" model with 2 phones fetched from xhr', function() {
+        expect(scope.phones).toBeUndefined();
+        $httpBackend.flush();
 
-        var dates = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        expect(scope.date).toBe(dates[(new Date()).getDay()]);
-    }));
+        expect(scope.phones).toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+    });
 
     it('should set the default value of orderProp model', function(){
         expect(scope.orderProp).toBe('age');
