2ff615519cfa6b5da73d64671479cecb12bfe52d
Ikuo Degawa <degawa@ikuwow.com>
2016-02-21 11:00:52 +0900

    ADd phone images and step-6 finished

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -6,21 +6,6 @@ describe('PhoneCat App', function() {
             browser.get('app/index.html');
         });
 
-        it('should filter the phone list as a user types into the search box', function() {
-            var phoneList = element.all(by.repeater('phone in phones'));
-            var query = element(by.model('query'));
-
-            expect(phoneList.count()).toBe(3);
-
-            query.sendKeys('nexus');
-            expect(phoneList.count()).toBe(1);
-
-            query.clear();
-            query.sendKeys('motorola');
-            expect(phoneList.count()).toBe(2);
-
-        });
-
         it('should display the current filter value in the title bar', function() {
 
             var query = element(by.model('query'));
@@ -32,29 +17,13 @@ describe('PhoneCat App', function() {
             expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
         });
 
-        it('should be possible to control phone order via the drop down select box', function(){
-            var phoneNameColumn = element.all(by.repeater('phone in phones').column('phone.name'));
+        it('should render phone specific links', function() {
             var query = element(by.model('query'));
-
-            function getNames() {
-                return phoneNameColumn.map(function(elm) {
-                    return elm.getText();
-                });
-            }
-
-            query.sendKeys('tablet');
-
-            expect(getNames()).toEqual([
-                "Motorola XOOM™ with Wi-Fi",
-                "MOTOROLA XOOM™"
-            ]);
-
-            element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
-
-            expect(getNames()).toEqual([
-                "MOTOROLA XOOM™",
-                "Motorola XOOM™ with Wi-Fi"
-            ]);
+            query.sendKeys('nexus');
+            element.all(by.css('.phones li a')).first().click();
+            browser.getLocationAbsUrl().then(function(url) {
+              expect(url).toBe('/phones/nexus-s');
+            });
         });
     });
 });
