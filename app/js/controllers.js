0ebd74d9decd46be6b516e57ee2987236c37e305
Ikuo Degawa <degawa@ikuwow.com>
2016-02-24 20:26:42 +0900

    Separate templates

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,10 +1,12 @@
 
-var phonecatApp = angular.module('phonecatApp', []); // html ng-app=...
+var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
+
     // $http is added, this is dependency injector
     var dates = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     $scope.date = dates[(new Date()).getDay()];
+
     $http.get('phones/phones.json').success(function(data){
         $scope.phones = data;
         // $scope.phones = data.splice(0, 5);
@@ -12,4 +14,11 @@ phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
 
     // default order property
     $scope.orderProp = 'age';
-});
+
+}]);
+
+
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
+}]);
